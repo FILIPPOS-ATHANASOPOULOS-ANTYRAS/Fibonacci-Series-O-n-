a98a937068c53947387008419e08ce120988f3aa
Philip <80889555+PhilipAth@users.noreply.github.com>
2022-08-12 15:05:44 +0300

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,17 +1,24 @@
 import java.lang.Math;
+
 class fibonacci{
-  public static Integer NthFibonacci(Integer n){
-    double fi = 1.618034;
-    int fb = (int) Math.round((Math.pow(fi,n) - Math.pow(1-fi,n))/Math.sqrt(5));
+  public static long NthFibonacci(int n){
+    double a = (1/Math.sqrt(5))*Math.pow(2, n);
+   
+    double radians1 = Math.toRadians(36.0);
+    double radians2 = Math.toRadians(108.0);
+    
+    double b = Math.pow(Math.cos(radians1), n) - Math.pow(Math.cos(radians2), n);
+
+    long fb = (long) Math.round(a*b);
     return fb;
   }
 
-  public static void FibonacciSeries(Integer n){
+  public static void FibonacciSeries(int n){
     for(int i = 0; i < n; i++){
-      System.out.print(NthFibonacci(i) + " ");
+      System.out.println( i + " : " +  NthFibonacci(i));
     }
   }  
   public static void main(String[] args) {
-    FibonacciSeries(10);
+    FibonacciSeries(1000);
   }
 }
